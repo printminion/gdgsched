d1e1afa589a685919eb06506862c182618152644
russenreaktor <m.kupriyanov@gmail.com>
2012-10-09 01:30:54 +0200

    fixed #4 - added GoogleAnalytics,

--- android/src/com/google/analytics/tracking/android/EasyTracker.java
+++ android/src/com/google/analytics/tracking/android/EasyTracker.java
@@ -16,32 +16,59 @@
 
 package com.google.analytics.tracking.android;
 
+import com.google.android.apps.analytics.GoogleAnalyticsTracker;
+import com.google.android.apps.iosched.Config;
+
 import android.app.Activity;
 import android.content.Context;
+import android.util.Log;
 
 /**
  * Temporarily just a stub.
  */
 public class EasyTracker {
+	
+	private GoogleAnalyticsTracker tracker;
+	
+	String TAG = "EasyTracker";
+	
     public static EasyTracker getTracker() {
         return new EasyTracker();
     }
+    
+    private EasyTracker() {
+    	Log.d(TAG, "EasyTracker");
+    	tracker = GoogleAnalyticsTracker.getInstance();
+        
+    }
 
     public void trackView(String s) {
+    	Log.d(TAG, "trackView");
+    	tracker.trackPageView(s);
     }
 
     public void trackActivityStart(Activity activity) {
+    	Log.d(TAG, "trackActivityStart:" + activity.getLocalClassName());
+    	tracker.trackPageView(activity.getLocalClassName());
     }
 
     public void trackActivityStop(Activity activity) {
+    	Log.d(TAG, "trackActivityStop");
+    	tracker.stopSession();
     }
 
     public void setContext(Context context) {
+    	Log.d(TAG, "setContext");
+    	tracker.startNewSession(Config.GOOGLE_ANALYTICS, 1, context.getApplicationContext());
     }
 
     public void trackEvent(String s1, String s2, String s3, long l) {
+    	Log.d(TAG, "trackEvent");
+    	tracker.trackEvent(s1, s2, s3, 0);
     }
 
     public void dispatch() {
+    	Log.e(TAG, "dispatch");
+    	tracker.dispatch();
     }
 }

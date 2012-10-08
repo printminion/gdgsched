ee318c6ddb6a9fbbb1c01bded6ae7c5d1d646701
russenreaktor <m.kupriyanov@gmail.com>
2012-10-08 22:32:08 +0200

    fixed #5 #7

--- scripts/increase_verbosity.sh
+++ scripts/increase_verbosity.sh
@@ -2,6 +2,9 @@
 # Remember VERBOSE only works on debug builds of the app
 adb shell setprop log.tag.gdgsched_SyncHelper VERBOSE
 adb shell setprop log.tag.gdgsched_SessionsHandler VERBOSE
+
+adb shell setprop log.tag.gdgsched_ScheduleProvider VERBOSE
+
 adb shell setprop log.tag.gdgsched_ImageCache VERBOSE
 adb shell setprop log.tag.gdgsched_ImageWorker VERBOSE
 adb shell setprop log.tag.gdgsched_ImageFetcher VERBOSE
